096d0ea178e0ccd27f9bbc4719369a7d8d8700da
abhayvel <quickitdotnet@gmail.com>
2022-03-26 13:39:58 +0530

    Added first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo';
+  title = 'Demo'; // Instance Variable 
+
+  static d: string;
+
+  name="Abhay";//
+  address: string="";
+  show(value: string){
+    this.name = value;
+    for (let i = 0; i < 5; i=i+1) {
+      console.log(i);
+    }
+
+   
+     
+    /*
+     * String< 30%
+     * number<-10%
+     * boolean<-5%
+     * DateTime<-15%
+     * Group/collection/Array<-40%
+     *
+     */
+  }
+
+  showConsoleNormalFor() {
+    let arr = [1, 4, 6, 3, 2, 1, 9];
+    for (let i = 0; i < arr.length; i = i + 1) {
+      console.log(`${i}-${arr[i]}`);
+    }
+  }
+
+
+  showConsoleForIn() {
+    let arr = [1, 4, 6, 3, 2, 1, 9];
+    for (let i in arr) {
+      console.log(`${i}-${arr[i]}`);
+    }
+  }
+
+  showConsoleForLetOf() {//ES 6 
+    let arr = [1, 4, 6, 3, 2, 1, 9];
+    for (let a of arr) { // you have to add index.
+      console.log(`${a}`);
+    }
+  }
+
+  showConsoleForEach() {//ES 6 
+    let arr = [1, 4, 6, 3, 2, 1, 9];
+    arr.forEach(function (e, i) {  //CallBack <- Deleget 
+      console.log(`${e}`);
+    });
+
+    
+  }
+
+
+  showConsoleForEach2() {//ES 6 
+    let arr = [1, 4, 6, 3, 2, 1, 9];
+    //arrow function ES 6
+    arr.forEach((e, i) => {
+      console.log(`${e}`);
+    });
+
+
+  }
 }
+
+//3 types
+//--> Instance Variable 
+//class Variable 
+//Local Variable/Parameter or argments 
